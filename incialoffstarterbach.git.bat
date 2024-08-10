5e28216fb5d5a41b31006a0aaca319bdd9fa529f
MrsIngridAlves <ingrid.alvesribeiro@hotmail.com>
2024-08-09 21:42:10 -0300

    Primeira PAgina WEB

---
+++ incialoffstarterbach.git.bat
@@ -0,0 +1,13 @@
+echo OFF
+
+echo --- Configurando Usuario GitHub ----
+
+git config --global user.name "MrsIngridAlves"
+git config --global user.email "ingrid.alvesribeiro@hotmail.com"
+echo --- mostrando configuração atual do GitHub no computador
+git config --list
+echo --- Fim ---
+echo --- don't give up now. Go ahead ---
+
+pause
+
